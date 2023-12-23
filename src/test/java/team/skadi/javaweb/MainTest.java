cfe8e5980a03fc72d9fe092431a6fa5db5c4d9cf
mizulk <1930764560@qq.com>
2023-12-23 14:48:39 +0800

    优化数据库连接测试类

--- src/test/java/team/skadi/javaweb/MainTest.java
+++ src/test/java/team/skadi/javaweb/MainTest.java
@@ -14,30 +14,42 @@ import java.io.IOException;
 public class MainTest {
 
 	@Test
-	public void testDataBase() {
+	public void setUpDataBase() {
 		String configPath = "src/main/resources/database-config.properties";
 		String templatePath = "src/main/resources/database-config-template.properties";
 		File file = new File(configPath);
-		if (!file.exists()) {
-			try {
+		try {
+			if (!file.exists()) {
+
 				String password = JOptionPane.showInputDialog("检测到无数据库配置文件\n请输入你的MySQL密码来生成配置文件：", "123456");
-				BufferedReader in = new BufferedReader(new FileReader(templatePath));
-				BufferedWriter out = new BufferedWriter(new FileWriter(file));
-				String line;
-				while ((line = in.readLine()) != null) {
-					if (line.contains("password"))
-						out.write("jdbc.password = " + password);
-					else
-						out.write(line + "\n");
+				writeFile(configPath, file, password);
+				JOptionPane.showMessageDialog(null, "创建配置文件成功，如果需要更改请再次运行本程序。");
+			} else {
+				int i = JOptionPane.showConfirmDialog(null, "已存在数据库配置文件，是否需要更改密码？", "请选择", JOptionPane.YES_NO_OPTION);
+				if (i == JOptionPane.YES_OPTION) {
+					String password = JOptionPane.showInputDialog("请输入你的MySQL密码来x修改配置文件：", "123456");
+					writeFile(configPath, file, password);
+					JOptionPane.showMessageDialog(null, "修改配置文件成功，如果需要再更改请再次运行本程序。");
 				}
-				out.flush();
-				out.close();
-				in.close();
-				JOptionPane.showMessageDialog(null, "创建配置文件成功，如果需要修改请到src/main/resources/mybatis-config.xml下修改密码");
-			} catch (IOException e) {
-				e.printStackTrace();
-				JOptionPane.showMessageDialog(null, "生成配置文件失败: " + e);
 			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			JOptionPane.showMessageDialog(null, "生成配置文件失败: " + e);
 		}
+	}
+
+	private void writeFile(String inputFile, File outPutFile, String password) throws IOException {
+		BufferedReader in = new BufferedReader(new FileReader(inputFile));
+		BufferedWriter out = new BufferedWriter(new FileWriter(outPutFile));
+		String line;
+		while ((line = in.readLine()) != null) {
+			if (line.contains("password"))
+				out.write("jdbc.password = " + password);
+			else
+				out.write(line + "\n");
+		}
+		out.flush();
+		out.close();
+		in.close();
 	}
 }
