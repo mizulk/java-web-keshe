c8a859ea0d9ce76ad196698c27efe40087afb0fc
mizulk <1930764560@qq.com>
2023-12-23 14:48:39 +0800

    优化数据库连接测试类

--- src/test/java/team/skadi/javaweb/MainTest.java
+++ src/test/java/team/skadi/javaweb/MainTest.java
@@ -22,13 +22,13 @@ public class MainTest {
 			if (!file.exists()) {
 
 				String password = JOptionPane.showInputDialog("检测到无数据库配置文件\n请输入你的MySQL密码来生成配置文件：", "123456");
-				writeFile(configPath, file, password);
+				writeFile(templatePath, file, password);
 				JOptionPane.showMessageDialog(null, "创建配置文件成功，如果需要更改请再次运行本程序。");
 			} else {
 				int i = JOptionPane.showConfirmDialog(null, "已存在数据库配置文件，是否需要更改密码？", "请选择", JOptionPane.YES_NO_OPTION);
 				if (i == JOptionPane.YES_OPTION) {
 					String password = JOptionPane.showInputDialog("请输入你的MySQL密码来x修改配置文件：", "123456");
-					writeFile(configPath, file, password);
+					writeFile(templatePath, file, password);
 					JOptionPane.showMessageDialog(null, "修改配置文件成功，如果需要再更改请再次运行本程序。");
 				}
 			}
